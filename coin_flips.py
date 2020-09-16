f2a10f2b329aeeebf09fe108235c00c9112e267a
Rome Bell <romebell5@gmail.com>
2020-09-16 10:43:30 -0700

    [coin_flips] Add last problem

---
+++ coin_flips.py
@@ -0,0 +1,14 @@
+# You will have to figure out what parameters to include
+# 🚨 All functions must use recursion 🚨
+
+# This function returns an array of all possible outcomes from flipping a coin N times.
+# Input type: Integer 
+# H stands for Heads and T stands for tails
+# Represent the two outcomes of each flip as "H" or "T"
+
+def coin_flips(n):
+    # Write code here
+    pass
+
+# print(coinFlips(2)) 
+# => ["HH", "HT", "TH", "TT"]
