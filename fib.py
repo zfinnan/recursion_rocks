ef63343ed3b41c5882d0dc23ef0d0a0f900b2a14
Rome Bell <romebell5@gmail.com>
2020-09-16 10:42:04 -0700

    [fib] Add problem

---
+++ fib.py
@@ -0,0 +1,21 @@
+# You will have to figure out what parameters to include
+# 🚨 All functions must use recursion 🚨
+
+# This function returns the Nth number in the fibonacci sequence.
+# https://en.wikipedia.org/wiki/Fibonacci_number
+# For this function, the first two fibonacci numbers are 1 and 1
+
+def fib(n):
+    # Write code here
+    pass
+
+# print(fib(-1))
+# => 0
+# print(fib(0))
+# => 0
+# print(fib(1))
+# => 1
+# print(fib(2))
+# => 1
+# print(fib(7))
+# => 13
