0c079ab9d8ba916b45dc10dcf7ec52220a19f8a8
Rome Bell <romebell5@gmail.com>
2020-09-16 10:35:18 -0700

    [reverse] Add problem

---
+++ reverse.py
@@ -0,0 +1,20 @@
+# Write code inside the functions
+# You will have to figure out what parameters to include
+# 🚨 All functions must use recursion 🚨
+
+# Write a recursive function called `reverse` that accepts a ss and returns a reversed ss.
+
+def reverse(ss):
+  # Write code here
+  pass
+
+# print(reverse("")) 
+# => ""
+# print(reverse("a")) 
+# => "a"
+# print(reverse("ab")) 
+# => "ba"
+# print(reverse("computer")) 
+# => "retupmoc"
+# print(reverse(reverse("computer"))) 
+# => "computer"
